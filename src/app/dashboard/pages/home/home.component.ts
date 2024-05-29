6ae875ed7ca7ad4ac8f8515aefb6839d17e52342
juancarlosG97 <juancarlosgomariz97@gmail.com>
2024-05-29 13:07:54 +0200

    develop home page

--- src/app/dashboard/pages/home/home.component.ts
+++ src/app/dashboard/pages/home/home.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { HomeService } from '../../../services/home.service';
+import { Weather } from '../../interface/weather.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  private homeService = inject(HomeService);
+  public weatherDate?: Weather;
+
+  ngOnInit(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+        this.homeService.getPredection(lat, long)
+          .subscribe((resp) => {
+            this.weatherDate = resp;
+          });
+      });
+    }
+  }
+
 
 }
---
+++ src/app/services/home.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../environments/environments';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+
+  private readonly apiKey: string = environment.apiKey;
+  private http = inject(HttpClient);
+
+  constructor() { }
+
+  getPredection(lat: any, log: any): Observable<any> {
+    /*const headers = new HttpHeaders()
+      .set('api_key', this.apiKey)
+    //.set('Access-Control-Allow-Origin', 'https://opendata.aemet.es');
+    //const url = 'https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/horaria/Monovar';
+    //const url = 'https://opendata.aemet.es/opendata/api/observacion/convencional/todas';
+    const url = 'http://api.weatherstack.com/';
+
+    return this.http.get(url, { headers });*/
+    const accesKey = this.apiKey;
+    const apiUrl = `http://api.weatherstack.com/current?access_key=${accesKey}&query=${lat},${log}`;
+    //const apiUrl = `http://api.weatherstack.com/autocomplete?access_key=${accesKey}&query=${city}`;
+    return this.http.get(apiUrl);
+
+  }
+}
